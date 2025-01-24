a5017d31ce74b83e48ebbc5ef4e7cc23675d3b86
Aluiagg104 <88749067+Aluiagg104@users.noreply.github.com>
2025-01-24 21:14:04 +0100

    1.0.0
    Die erste Version von EasyNotenManager

---
+++ 1.0.0/EasyNotenManager/EasyNotenManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyNotenManagerApp.swift
+//  EasyNotenManager
+//
+//  Created by Oliver Henkel on 24.01.25.
+//
+
+import SwiftUI
+
+@main
+struct EasyNotenManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 1.0.0/EasyNotenManagerTests/EasyNotenManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyNotenManagerTests.swift
+//  EasyNotenManagerTests
+//
+//  Created by Oliver Henkel on 24.01.25.
+//
+
+import Testing
+@testable import EasyNotenManager
+
+struct EasyNotenManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
